ea6e9a330fbd3700fac818c715a6d871767d4300
Francesco Maida <francesco.maida@gmail.com>
2026-03-25 16:33:15 +0100

    Sistemato il funzionamento del form di contatto, che prima non andava

--- static/assets/js/contact.js
+++ static/assets/js/contact.js
@@ -2,51 +2,45 @@ $(function () {
 
   "use strict";
 
-  // Inizializza il validator
-  $('#contact-form').validator();
-
   // Quando il form viene inviato
   $('#contact-form').on('submit', function (e) {
     e.preventDefault(); // evita submit tradizionale
     const $form = $(this);
 
-    // Se il validator non blocca l'invio
-    if (!e.isDefaultPrevented()) {
-      const url = "https://formspree.io/f/mrbbvgvq";
+    const url = "https://formspree.io/f/mrbbvgvq";
 
-      $.ajax({
-        type: "POST",
-        url: url,
-        data: $form.serialize(),
-        headers: { "Accept": "application/json" },
-        dataType: "json"
-      })
-      .done(function () {
-        const alertBox = `
-          <div class="alert alert-success alert-dismissable">
-            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-            Messaggio inviato correttamente.
-          </div>`;
-        $form.find('.messages').html(alertBox);
-        $form[0].reset();
-      })
-      .fail(function (jqXHR) {
-        let msg = 'Si è verificato un errore durante l\'invio. Riprova più tardi.';
-        try {
-          const resp = jqXHR.responseJSON;
-          if (resp && resp.errors && resp.errors.length) {
-            msg = resp.errors.map(e => e.message).join(' ');
-          }
-        } catch (_) {}
-        const alertBox = `
-          <div class="alert alert-danger alert-dismissable">
-            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-            ${msg}
-          </div>`;
-        $form.find('.messages').html(alertBox);
-      });
+    $.ajax({
+      type: "POST",
+      url: url,
+      data: $form.serialize(),
+      headers: { "Accept": "application/json" },
+      dataType: "json"
+    })
+    .done(function () {
+      const alertBox = `
+        <div class="alert alert-success alert-dismissable">
+          <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+          Messaggio inviato correttamente.
+        </div>`;
+      $form.find('.messages').html(alertBox);
+      $form[0].reset();
+    })
+    .fail(function (jqXHR) {
+      let msg = 'Si è verificato un errore durante l\'invio. Riprova più tardi.';
+      try {
+        const resp = jqXHR.responseJSON;
+        if (resp && resp.errors && resp.errors.length) {
+          msg = resp.errors.map(e => e.message).join(' ');
+        }
+      } catch (_) {}
+      const alertBox = `
+        <div class="alert alert-danger alert-dismissable">
+          <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+          ${msg}
+        </div>`;
+      $form.find('.messages').html(alertBox);
+    });
 
-      return false;
-    }
+    return false;
   });
 });
